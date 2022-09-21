9b71b74c17e5dc79cebc6833ea77925147dd0209
Chenhao Xue <xch927027@pku.edu.cn>
2022-09-21 09:17:08 +0800

    hyper graph generator

--- dataset/graph_generator/base.py
+++ dataset/graph_generator/base.py
@@ -3,11 +3,9 @@ import sys
 sys.path.append("..")
 
 import pickle as pkl
-import dgl
-import numpy as np
 import torch
-from scipy import stats
 from collections import UserDict
+import networkx as nx
 
 from trace_parser.trace_parser import TraceParser
 
@@ -28,8 +26,27 @@ class SmartDict(UserDict):
         return self.__cnt
 
 
+def binarize_float(tensor, bit):
+    """Binarize float into integer, represented in vector
+    Input: tensor(*)
+    Return: tensor(*, bit)
+    """
+    quantity_tensors = []
+
+    for i in range(bit):
+        q = tensor % (2 ** (i+1))
+        q = q // (2 ** i)
+        q = q.unsqueeze(-1)
+        quantity_tensors.append(q)
+
+    bin_tensor = torch.stack(quantity_tensors, dim=-1).float()
+    return bin_tensor
+
+
 class GraphGenerator():
-    """Generating graph from parser."""
+    """Generating graph from parser.
+    This is simply a base class.
+    """
 
     def __init__(self, parser: TraceParser, predict: bool) -> None:
         self.parser = parser
@@ -38,12 +55,14 @@ class GraphGenerator():
     def generate_graph(self, layer=None, batch=None):
         """Generate subgraph if layer and batch are given, else the whole graph.
         """
-        graph = self.__gen_graph(layer, batch)
+        G = self.__load_graph()
+        graph = self._gen_graph(G)
         return graph
 
     def generate_label(self, layer=None, batch=None):
-        assert self.predict == False
-        label = self.__gen_label(layer, batch)
+        assert self.predict == False, "Graph generator: no access to label during prediction."
+        G = self.__load_graph(layer, batch)
+        label = self._gen_label(G)
         return label
 
     def save_data(self, save_path, graph, label=None):
@@ -51,213 +70,21 @@ class GraphGenerator():
         with open(save_path, "wb") as f:
             pkl.dump(saving, f)
 
-    def __gen_graph(self, layer=None, batch=None):
-        G = self.parser.graph_parser.get_graph(layer, batch)
-
-        graph, pkt2id, rt2id = self.__gen_hetero_graph(G)
-        for attr, t in self.__gen_hyper_nattr(G, pkt2id).items():
-            graph.nodes['packet'].data[attr] = t
-        for attr, t in self.__gen_nattr(G, rt2id).items():
-            graph.nodes['router'].data[attr] = t
-
-        return graph
-
-    def __gen_hetero_graph(self, G):
-        """Generate heterograph given the subgraph.
-        Return: dgl.heterograph, pkt2id, rt2id
-        """
-        packet_to_router = dict()  # pid: rids
-        router_to_router = dict()  # rid: rids
-
-        pkt2id = SmartDict()
-        rt2id = SmartDict()
-
-        for u, v, eattr in G.edges(data=True):
-            if eattr['edge_type'] != "data":
-                continue
-            pkt = eattr["pkt"][0]  # the first pkt is fine
-            u_pe, v_pe = G.nodes[u]['p_pe'], G.nodes[v]['p_pe']
-            routing = self.parser.routing_parser.get_routing_hops(u_pe, v_pe, pkt)
-
-            pid = pkt2id[pkt]
-            rid_u = rt2id[u_pe]
-            packet_to_router[pid] = [rid_u]  # add routing start
-
-            for s, e in routing:
-                rid_s, rid_e = rt2id[s], rt2id[e]
-                packet_to_router[pid].append(rid_e)  # add every routing hop's end
-
-                # for every hop, add physical channel connection
-                if rid_s not in router_to_router.keys():
-                    router_to_router[rid_s] = []
-                if rid_e not in router_to_router[rid_s]:
-                    router_to_router[rid_s].append(rid_e)
-
-        pass_edges = self.__parse_edges_dict(packet_to_router)
-        transfer_edges = (pass_edges[1].clone().detach(), pass_edges[0].clone().detach())
-        connect_edges = self.__parse_edges_dict(router_to_router)
-        backpressure_edges = (connect_edges[1].clone().detach(), connect_edges[0].clone().detach())
-        graph = dgl.heterograph({
-            ('packet', 'pass', 'router'): pass_edges,
-            ('router', 'transfer', 'packet'): transfer_edges,
-            ('router', 'connect', 'router'): connect_edges,
-            ('router', 'backpressure', 'router'): backpressure_edges,
-        })
-
-        return graph, pkt2id, rt2id
-
-    def __parse_edges_dict(self, edges):
-        """ Parse dict of edge lists.
-        Return: source node tensor, dest node tensor
-        """
-        src, dst = [], []
-        for s, l in edges.items():
-            for d in l:
-                src.append(s)
-                dst.append(d)
-
-        return torch.tensor(src), torch.tensor(dst)
-
-    def __gen_nattr(self, G, rt2id):
-        """Generate router's attribute.
-        Return: [Tensor(#Router, node_attr_dim)]
-
-        Node attribute contains:
-        - op_type: one-hot representation, dim=4
-        """
-
-        num_routers =  len(rt2id)
-        op_type = torch.zeros(num_routers, 4)
-
-        cast_op_type = {
-            "wsrc": torch.tensor([1, 0, 0, 0]).unsqueeze(0),
-            "insrc": torch.tensor([0, 1, 0, 0]).unsqueeze(0),
-            "worker": torch.tensor([0, 0, 1, 0]).unsqueeze(0),
-            "sink": torch.tensor([0, 0, 0, 1]).unsqueeze(0),
-        }
-
-        for u, nattr in G.nodes(data=True):
-            u_pe = nattr['p_pe']
-            rid_u = rt2id[u_pe]
-            op_type[rid_u:rid_u+1, :] = cast_op_type[nattr['op_type']]
-        
-        return {
-            "op_type": op_type.float()
-        }
-
-
-    def __gen_hyper_nattr(self, G, pkt2id):
-        """Generate packet's attribute.
-        Return: [Tensor(#Packet, hyper_node_dim)
-
-        Node attribute contains:
-        - frequency to send packet, dim=1
-        - flit, dim=32
-        """
-        num_packets = len(pkt2id)
-        freq = torch.zeros(num_packets, 1)
-        flit = torch.zeros(num_packets, 1)
-
-        for u, v, eattr in G.edges(data=True):
-            if eattr['edge_type'] != "data":
-                continue
-            pkt = eattr["pkt"][0]  # the first pkt is fine
-            pid = pkt2id[pkt]
-            flit[pid:pid+1, :] = eattr['size']
-            freq[pid:pid+1, :] = 1 / G.nodes[u]['delay']
-
-        flit = self.__binarize_float(flit, 32)
-
-        return {
-            "flit": flit.float(),
-            "freq": freq.float(),
-        }
-
-    def __binarize_float(self, tensor, bit):
-        """Binarize float into integer, represented in vector
-        Input: tensor(N, 1)
-        Return: tensor(N, bit)
-        """
-        
-        assert len(tensor.shape) == 2
-        assert tensor.shape[1] == 1
-        N, _ = tensor.shape
-        bin_tensor = torch.zeros(N, bit)
-
-        for i in range(bit):
-            bin_tensor[:, i:i+1] = tensor % (2 ** (i+1))
-        return bin_tensor
-
-    def __gen_label(self, G):
-        """Calculate congestion ratio.
-        Return: Tensor(1,)
-        """
-        raise NotImplementedError("get latency from parser, not edges.")
-        #FIXME: get latency from parser, not edges.
-
-        wsrc = [n for n, attr in G.nodes(data=True) if attr["op_type"] == "wsrc"]
-        assert len(wsrc) == 1
-        wsrc = wsrc[0]
-        insrc = [n for n, attr in G.nodes(data=True) if attr["op_type"] == "insrc"]
-        assert len(insrc) == 1
-        insrc = insrc[0]
-        workers = [n for n, attr in G.nodes(data=True) if attr["op_type"] == "worker"]
-        assert len(workers) > 0
-    
-        w_cnt = int(G.nodes[wsrc]["cnt"])
-        in_cnt = int(G.nodes[insrc]["cnt"])
-        w_start = [float("inf")] * w_cnt
-        w_end = [-float("inf")] * w_cnt
-        in_start = [float("inf")] * in_cnt
-        in_end = [-float("inf")] * in_cnt
-
-        for w in workers:
-            w_edges = G.edges[wsrc, w]["pkt"]
-            w_pids = sorted(list(w_edges.keys()))
-
-            in_edges = G.edges[insrc, w]["pkt"]
-            in_pids = sorted(list(in_edges.keys()))
-
-            for t in range(w_cnt):
-                w_pkt = w_edges[w_pids[t]]
-                w_start[t] = min(w_start[t], w_pkt["start_cycle"])
-                w_end[t] = max(w_end[t], w_pkt["end_cycle"])
-
-            for t in range(in_cnt):
-                in_pkt = in_edges[in_pids[t]]
-                in_start[t] = min(in_start[t], in_pkt["start_cycle"])
-                in_end[t] = max(in_end[t], in_pkt["end_cycle"])
-
-        # align different cnts
-        if w_cnt > in_cnt:
-            ratio = w_cnt // in_cnt
-            w_latency = {i: l[1]-l[0] for i, l in enumerate(zip(w_start, w_end))}
-            in_latency = {ratio*i: l[1]-l[0] for i, l in enumerate(zip(in_start, in_end))}
-        elif w_cnt < in_cnt:
-            ratio = in_cnt // w_cnt
-            w_latency = {ratio*i: l[1]-l[0] for i, l in enumerate(zip(w_start, w_end))}
-            in_latency = {i: l[1]-l[0] for i, l in enumerate(zip(in_start, in_end))}
+    def __load_graph(self, layer=None, batch=None):
+        if layer != None and batch != None:
+            G = self.parser.graph_parser.get_graph(layer, batch)
         else:
-            w_latency = {i: l[1]-l[0] for i, l in enumerate(zip(w_start, w_end))}
-            in_latency = {i: l[1]-l[0] for i, l in enumerate(zip(in_start, in_end))}
+            if layer != None:
+                raise Warning(f"Graph Generator: layer {layer} without specific batch, \
+                    use whole graph instead.")
+            if batch != None:
+                raise Warning(f"Graph Generator: batch {batch} without specific layer, \
+                    use whole graph instead.")
+            G = self.parser.graph_parser.get_graph(None, None)
+        return G
 
-        # Congestion is measured by a latency ratio.
-        w_x = np.array(list(w_latency.keys()))
-        w_y = np.array(list(w_latency.values()))
-        in_x = np.array(list(in_latency.keys()))
-        in_y = np.array(list(in_latency.values()))
-        
-        # TODO: We ignore cases when number of iteration is small
-        # ignore the first irregular data due to instr. flow
-        w_congestion = max(stats.linregress(w_x[1:], w_y[1:]).slope, 0) if len(w_x) > 4 else 0
-        in_congestion = max(stats.linregress(in_x[1:], in_y[1:]).slope, 0) if len(in_x) > 4 else 0
+    def _gen_graph(self, G:nx.DiGraph):
+        raise NotImplementedError("Base Graph Generator doesn't implement _gen_graph")
 
-        # adjust workload ratio
-        workload = min(int(G.nodes[wsrc]['delay']), int(G.nodes[insrc]['delay']))
-        w_congestion = w_congestion / workload
-        in_congestion = in_congestion / workload
-
-        # normalize
-        congestion = max(w_congestion, in_congestion)
-        congestion = torch.tensor(congestion)
-        return congestion
+    def _gen_label(self, G:nx.DiGraph):
+        raise NotImplementedError("Base Graph Generator doesn't implement _gen_label")
---
+++ dataset/graph_generator/hyper.py
@@ -0,0 +1,161 @@
+import os
+import sys
+from xml.dom.minicompat import NodeList
+sys.path.append("..")
+
+import networkx as nx
+import torch
+import dgl
+
+from base import GraphGenerator, SmartDict, binarize_float
+
+class HyperGraphGenerator(GraphGenerator):
+
+    def _gen_graph(self, G:nx.DiGraph):
+        graph, pkt2id, rt2id = self.__gen_skeleton(G)
+        for attr, t in self.__gen_hyper_nattr(G, pkt2id).items():
+            graph.nodes['packet'].data[attr] = t
+        for attr, t in self.__gen_nattr(G, rt2id).items():
+            graph.nodes['router'].data[attr] = t
+
+        return graph
+
+    def _gen_label(self, G:nx.DiGraph):
+        pass
+
+    def __gen_skeleton(self, G:nx.DiGraph):
+        """Generate heterograph given the subgraph.
+        Return: dgl.heterograph, pkt2id, rt2id
+        """
+        packet_to_router = dict()  # pid: rids
+        router_to_router = dict()  # rid: rids
+
+        pkt2id = SmartDict()
+        rt2id = SmartDict()
+
+        for u, v, eattr in G.edges(data=True):
+            if eattr['edge_type'] != "data":
+                continue
+            pkt = eattr["pkt"][0]  # the first pkt is fine
+            u_pe, v_pe = G.nodes[u]['p_pe'], G.nodes[v]['p_pe']
+            routing = self.parser.routing_parser.get_routing_hops(u_pe, v_pe, pkt)
+
+            pid = pkt2id[pkt]
+            rid_u = rt2id[u_pe]
+            packet_to_router[pid] = [rid_u]  # add routing start
+
+            for s, e in routing:
+                rid_s, rid_e = rt2id[s], rt2id[e]
+                packet_to_router[pid].append(rid_e)  # add every routing hop's end
+
+                # for every hop, add physical channel connection
+                if rid_s not in router_to_router.keys():
+                    router_to_router[rid_s] = []
+                if rid_e not in router_to_router[rid_s]:
+                    router_to_router[rid_s].append(rid_e)
+
+        pass_edges = self.__parse_edges_dict(packet_to_router)
+        transfer_edges = (pass_edges[1].clone().detach(), pass_edges[0].clone().detach())
+        connect_edges = self.__parse_edges_dict(router_to_router)
+        backpressure_edges = (connect_edges[1].clone().detach(), connect_edges[0].clone().detach())
+        graph = dgl.heterograph({
+            ('packet', 'pass', 'router'): pass_edges,
+            ('router', 'transfer', 'packet'): transfer_edges,
+            ('router', 'connect', 'router'): connect_edges,
+            ('router', 'backpressure', 'router'): backpressure_edges,
+        })
+
+        return graph, pkt2id, rt2id
+
+    def __parse_edges_dict(self, edges):
+        """ Parse dict of edge lists.
+        Return: source node tensor, dest node tensor
+        """
+        src, dst = [], []
+        for s, l in edges.items():
+            for d in l:
+                src.append(s)
+                dst.append(d)
+
+        return torch.tensor(src), torch.tensor(dst)
+
+    def __gen_nattr(self, G, rt2id):
+        """Generate router's attribute.
+        Return: [Tensor(#Router, node_attr_dim)]
+
+        Node attribute contains:
+        - op_type: one-hot representation, dim=4
+        """
+
+        num_routers =  len(rt2id)
+        op_type = torch.zeros(num_routers, 4)
+
+        cast_op_type = {
+            "wsrc": torch.tensor([1, 0, 0, 0]).unsqueeze(0),
+            "insrc": torch.tensor([0, 1, 0, 0]).unsqueeze(0),
+            "worker": torch.tensor([0, 0, 1, 0]).unsqueeze(0),
+            "sink": torch.tensor([0, 0, 0, 1]).unsqueeze(0),
+        }
+
+        for u, nattr in G.nodes(data=True):
+            u_pe = nattr['p_pe']
+            rid_u = rt2id[u_pe]
+            op_type[rid_u:rid_u+1, :] = cast_op_type[nattr['op_type']]
+        
+        return {
+            "op_type": op_type.float()
+        }
+
+
+    def __gen_hyper_nattr(self, G, pkt2id):
+        """Generate packet's attribute.
+        Return: [Tensor(#Packet, hyper_node_dim)
+
+        Node attribute contains:
+        - freq: frequency to send packet, dim=1
+        - flit: size of flit (binarized float) dim=32
+        """
+        num_packets = len(pkt2id)
+        freq = torch.zeros(num_packets, 1)
+        flit = torch.zeros(num_packets, 1)
+
+        for u, v, eattr in G.edges(data=True):
+            if eattr['edge_type'] != "data":
+                continue
+            pkt = eattr["pkt"][0]  # the first pkt is fine
+            pid = pkt2id[pkt]
+            flit[pid:pid+1, :] = eattr['size']
+            freq[pid:pid+1, :] = 1 / G.nodes[u]['delay']
+
+        flit = binarize_float(flit, 32)
+
+        return {
+            "flit": flit.float(),
+            "freq": freq.float(),
+        }
+
+    def _gen_label(self, G: nx.DiGraph):
+        """Calculate maximum congestion ratio.
+        Use throughput to derive it.
+        Return: Tensor(1,)
+        """
+        srcs = [n for n, op_type in G.nodes(data="op_type")
+                if op_type == 'wsrc' or op_type == 'insrc']
+        workers  = [n for n, op_type in G.nodes(data="op_type")
+                if op_type == 'worker']
+
+        max_slope = 0  # throughput \mu = #cnt / #delay_total
+                       # k = \lambda / \mu = #delay_total / (#cnt * #delay)
+        for s in srcs:
+            cnt = G.nodes[s]['cnt']
+            delay = G.nodes[s]['delay']
+
+            for w in workers:
+                p = self.parser.outlog_parser
+                end_cycles = map(lambda pid: p.get_latency(s, w, pid)["end_cycle"],
+                                 G.edges[s, w]['pkt'])
+                duration = max(end_cycles) - min(end_cycles)
+                slope = duration / (cnt * delay)
+                max_slope = max(max_slope, slope)
+
+        return torch.tensor(max_slope)
