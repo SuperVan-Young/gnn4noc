b86b2aea4a3b7f120f9edc004689c29a7185b5f0
Chenhao Xue <xch927027@pku.edu.cn>
2022-10-06 10:21:00 +0800

    visualizer: mapping

--- dataset/trace_parser/visualizer.py
+++ dataset/trace_parser/visualizer.py
@@ -2,16 +2,18 @@ import os
 import sys
 
 import re
+from trace import Trace
+from turtle import width
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+from pandas import array
 
-from outlog_parser import OutlogParser
-from graph_parser import GraphParser
+from trace_parser import TraceParser
 
 class OpGraphVisualizer:
 
-    def __init__(self, save_path, graph_path, outlog_path=None) -> None:
+    def __init__(self, save_path, trace_parser:TraceParser) -> None:
         """
         Visualize Computation graph in multipartite fashion.
 
@@ -20,11 +22,10 @@ class OpGraphVisualizer:
         outlog_path: path of out.log, if you need latency information on your graph
         """
         self.save_path = save_path
-        self.graph_parser = GraphParser(graph_path)
-        self.outlog_parser = OutlogParser(outlog_path) if outlog_path else None
+        self.trace_parser = trace_parser
 
     def plot(self):
-        G = self.graph_parser.get_graph()
+        G = self.trace_parser.graph_parser.get_graph()
 
         # add virtual layer subset to nodes
         def extract_layer_number(s):
@@ -79,6 +80,63 @@ class OpGraphVisualizer:
         plt.savefig(self.save_path)
         plt.clf()
 
+    def plot_mapping(self, layer_name:str):
+        G = self.trace_parser.graph_parser.get_graph()
+        nodes = [u for u, nattr in G.nodes(data=True)
+                if nattr["layer"] == layer_name and nattr["batch"] == 0]
+        G = G.subgraph(nodes)
+        array_size = self.trace_parser.spec_parser.get_array_size()
+
+        assert len([u for u, nattr in G.nodes(data=True) if nattr["op_type"] == "sink"]) == 1
+
+        plt.figure(figsize=[array_size*0.2, array_size*0.2])
+        plt.xticks(range(0, array_size-1, 2))
+        plt.yticks(range(0, array_size-1, 2))
+        plt.xlim(-1, array_size)
+        plt.ylim(-1, array_size)
+
+        # draw nodes
+        node_color_map = {
+            "wsrc": 0,
+            "insrc": 0.25,
+            "worker": 0.5,
+            "sink": 0.75
+        }
+        y = [p_pe // array_size  for _, p_pe in G.nodes(data="p_pe")]
+        x = [p_pe % array_size  for _, p_pe in G.nodes(data="p_pe")]
+        c = [node_color_map[node_type] for _, node_type in G.nodes(data="op_type")]
+
+        plt.scatter(x, y, c=c, s=50, cmap=plt.get_cmap("Dark2"), marker="x")
+
+        # draw edges
+        node2pe = lambda u: G.nodes[u]['p_pe']
+        node2pos = lambda u: (G.nodes[u]["p_pe"] % array_size, G.nodes[u]["p_pe"] // array_size)
+        channel2cnt = dict()
+        
+        for u, v, eattr in G.edges(data=True):
+            if eattr['edge_type'] != "data" or len(eattr["pkt"]) == 0:
+                continue  # could happen when insrc and worker on the same pe
+            pkt = eattr["pkt"][0]
+            u_pe, v_pe = node2pe(u), node2pe(v)
+            channels = self.trace_parser.routing_parser.get_routing_hops(u_pe, v_pe, pkt)
+
+            for s, d in channels:
+                if (s, d) not in channel2cnt.keys():
+                    channel2cnt[(s, d)] = 1
+                else:
+                    channel2cnt[(s, d)] += 1
+
+        pe2pos = lambda x: (x % array_size, x // array_size)
+        max_cnt = max(channel2cnt.values())
+        for chan, cnt in channel2cnt.items():
+            s, d = chan
+            s_pos, d_pos = pe2pos(s), pe2pos(d)
+            plt.arrow(s_pos[0], s_pos[1], d_pos[0]-s_pos[0], d_pos[1]-s_pos[1], width=0.05, color='r', alpha=cnt/max_cnt)
+            plt.text((s_pos[0]+d_pos[0])/2 - 0.2, (s_pos[1]+d_pos[1])/2 - 0.2, str(cnt), fontsize=7)
+
+        plt.savefig(self.save_path)
+        plt.clf()
+
     def _get_node_label(self, node_type:str):
         """
         Returns:
@@ -108,12 +166,12 @@ class OpGraphVisualizer:
         edge_label = dict()
 
         if edge_type == "data":
-            assert self.outlog_parser != None
+            assert self.trace_parser.outlog_parser != None
             def get_last_packet_latency(u, v):
                 u_pe, v_pe = G.nodes[u]['p_pe'], G.nodes[v]['p_pe']
                 pkt = G.edges[u, v]["pkt"]
                 if len(pkt) > 0:
-                    lat = self.outlog_parser.get_latency(u_pe, v_pe, max(pkt))
+                    lat = self.trace_parser.outlog_parser.get_latency(u_pe, v_pe, max(pkt))
                     lat = lat['end_cycle'] - lat['start_cycle']
                     lat = int(lat)
                 else:
@@ -138,12 +196,12 @@ class OpGraphVisualizer:
 
         if edge_type == "data":
             # for example, color depth indicate a flow's last packet latency
-            assert self.outlog_parser != None
+            assert self.trace_parser.outlog_parser != None
             def get_last_packet_latency(u, v):
                 u_pe, v_pe = G.nodes[u]['p_pe'], G.nodes[v]['p_pe']
                 pkt = G.edges[u, v]["pkt"]
                 if len(pkt) > 0:
-                    lat = self.outlog_parser.get_latency(u_pe, v_pe, max(pkt))
+                    lat = self.trace_parser.outlog_parser.get_latency(u_pe, v_pe, max(pkt))
                     lat = lat['end_cycle'] - lat['start_cycle']
                 else:
                     lat = 0
@@ -165,6 +223,14 @@ if __name__ == "__main__":
     taskname = "bert-large_b1w1024_32x32"
     graph_path = "/home/xuechenhao/focus_scheduler/buffer/op_graph/op_graph_" + taskname + ".gpickle"
     outlog_path = "/home/xuechenhao/focus_scheduler/simulator/tasks/" + taskname + "/out.log"
+    routing_path = "/home/xuechenhao/focus_scheduler/simulator/tasks/" + taskname + "/routing_board"
+    spec_path = "/home/xuechenhao/focus_scheduler/simulator/tasks/" + taskname + "/spatial_spec"
+    trace_parser = TraceParser(
+        graph_path,
+        outlog_path,
+        routing_path,
+        spec_path,
+    )
 
-    visualizer = OpGraphVisualizer(save_path, graph_path, outlog_path)
-    visualizer.plot()
+    visualizer = OpGraphVisualizer(save_path, trace_parser)
+    visualizer.plot_mapping("bert-large_layer1")
